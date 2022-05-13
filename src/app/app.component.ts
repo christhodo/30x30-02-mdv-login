c3dceb582173607ce5673e4a6454c54ed88166fc
christhodo <102981895+christhodo@users.noreply.github.com>
2022-05-13 08:18:59 -0700

    add-icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mdv-login';
+  links = [
+    { path: '/login', icon: 'vpn_key', title: 'Login' },
+    { path: '/players', icon: 'view_list', title: 'Players' },
+  ];
 }
